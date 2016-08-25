d5d34b61a02a39f506d30ff6087e1f535c397f5f
nfbockhorst <nfbockhorst@gmail.com>
2016-08-25 14:15:28 -0500

    add control class for Sabertooth 2x32 over serial

---
+++ MotorControl.py
@@ -0,0 +1,39 @@
+"""	Purpose
+	Provides platform to control Sabertooth motor driver through Raspberry Pi 
+	serial port.
+	forward, reverse, left, right - speed passed to these methods only needs to
+	be positive.
+	motorLeft and motorRight is for manual driving and must be a value between
+	-2047 to 2047.
+"""
+
+"""	Prerequisites
+	Disable Raspberry Pi OS access to the UART (serial) port using raspi-config.
+"""
+
+import serial
+
+class MotorControl():
+	port = serial.Serial("/dev/ttyS0", baudrate = 9600, timeout = 30.0)
+	RTN = "\r\n"
+	
+	def forward(self, speed):
+		self.port.write("M1: " + str(speed) + self.RTN + "M2: " + str(speed) + self.RTN)
+	
+	def reverse(self, speed):
+		self.port.write("M1: " + "-" + str(speed) + self.RTN + "M2: " + "-" + str(speed) + self.RTN)
+	
+	def coast(self):
+		self.port.write("M1: 0" + self.RTN + "M2: 0" + self.RTN)
+	
+	def left(self, speed):
+		self.port.write("M1: " + "-" + str(speed) + self.RTN + "M2: " + str(speed) + self.RTN)
+	
+	def right(self, speed):
+		self.port.write("M1: " + str(speed) + self.RTN + "M2: " + "-" + str(speed) + self.RTN)
+	
+	def motorLeft(self, speed):
+		self.port.write("M1: " + str(speed) + self.RTN)
+	
+	def motorRight(self, speed):
+		self.port.write("M2: " + str(speed) + self.RTN)
